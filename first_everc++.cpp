62bd57c105347aab4313dc676e7fb401f3a5ab48
Talha <talhaozturk984@gmail.com>
2025-07-28 16:45:19 +0000

    Derleme Eklem first code c++

---
+++ first_everc++.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    //int sayi = 42;
+
+    //std::cout << sayi <<std::endl;
+
+    //char harf = 'Z';
+    //std::cout << harf << std::endl;
+    
+    //std::string selam = "Selam GPT";
+    //std::cout << selam << std::endl;
+
+    //std::string isim = "Talha";
+    //int yas = 18;D
+    //std::cout << "Benim adim " << isim << " ve yasim " << yas << std::endl;
+    
+    int sayi1 = 12;
+    int sayi2 = 23;
+    int toplam_sayi = sayi1 + sayi2;
+
+    std::cout << toplam_sayi << std::endl;
+}
